9a52839a10c7d79f04ad5634347a1b541df72401
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-10-31 19:09:42 -0300

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center bg-accent py-3 text-sm">
+      © 2023 Copyright&nbsp; <span className="font-semibold"> Store</span>
+    </div>
+  );
+};
+
+export default Footer;
