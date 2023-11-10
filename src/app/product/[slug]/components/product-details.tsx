10325948e0b0e622c74924100296888e6659acbc
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-11-10 01:24:53 -0300

    feat: add toast message

--- src/app/product/[slug]/components/product-details.tsx
+++ src/app/product/[slug]/components/product-details.tsx
@@ -2,20 +2,16 @@
 import PercentageBadge from "@/components/percentage-badge";
 import { Button } from "@/components/ui/button";
 
-import {
-  ProductWithTotalPrice,
-  computeProductTotalPrice,
-} from "@/helpers/productPrice";
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/productPrice";
+import cartProducts from "@/providers/cart-provider";
+import AddProductToCart from "@/requests/add-product-to-cart";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useState } from "react";
 import ProductDescription from "./product-description";
-import { TruckIcon } from "lucide-react";
-import Image from "next/image";
-import cartProducts from "@/providers/cart-provider";
-import { useSession } from "next-auth/react";
-import { v4 as uuidv4 } from "uuid";
-import GetProductsFromCart from "@/requests/get-products-from-cart";
-import AddProductToCart from "@/requests/add-product-to-cart";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { useRouter } from "next/navigation";
 
 export interface DataUser {
   id?: string | undefined;
@@ -35,6 +31,8 @@ const ProductDetails = ({ product }: { product: ProductWithTotalPrice }) => {
   const { data, status } = useSession();
   const dataUser = data as { user: DataUser };
   const [quantity, setQuantity] = useState(1);
+  const { toast } = useToast();
+  const router = useRouter();
 
   const addProduct = cartProducts((state) => state.addProduct);
   const handleQuantity = (status: string) => {
@@ -54,14 +52,24 @@ const ProductDetails = ({ product }: { product: ProductWithTotalPrice }) => {
       //salvar no localStorage
     }
     if (!userId || userId === undefined) {
-      return alert("Ocorreu um erro, atualize a página e tente novamente");
+      return alert("Ocorreu um erro, você está deslogado");
     }
     AddProductToCart({ userId, productId: product.id, quantity });
     addProduct({ product: product, productId: product.id, userId, quantity });
+    toast({
+      className: "bg-primary !bottom-0",
+      title: "Produto adicionado com sucesso!",
+      duration: 2000,
+      action: (
+        <ToastAction onClick={() => router.push("/cart")} altText="Carrinho">
+          Carrinho
+        </ToastAction>
+      ),
+    });
   };
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4 ">
+      <div className=" flex flex-col gap-2">
         <div className="flex flex-col gap-1">
           <p className="text-base text-primary">Disponível em estoque</p>
           <p className="text-xl font-semibold">{product.name}</p>
