1fe8be8ef20361cf8c0b05cc7a8b9fb3fc8c8452
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-11-27 17:04:00 -0300

    refactor: copyright year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,12 @@
 import { Separator } from "./ui/separator";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <>
       <Separator />
       <div className="flex items-center justify-center bg-accent py-3 text-sm opacity-60">
-        © 2023 Copyright&nbsp; <span className="font-semibold"> Store</span>
+        © {year} Copyright&nbsp; <span className="font-semibold"> Store</span>
       </div>
     </>
   );
